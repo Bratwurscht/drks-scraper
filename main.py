802eaa76d2d59d5cf2965b1c8ab1833e89fc51cb
bratwurscht <br4twurscht@gmail.com>
2015-11-21 21:26:48 +0100

    further scraper fields

--- main.py
+++ main.py
@@ -15,14 +15,118 @@ def main():
         # website = website.split('\n')
         my_scraper = Scraper()
         my_scraper.feed(website)
-        print(my_scraper.list)
-        for i in range(0, len(my_scraper.list)):
-            my_scraper.list[i]=my_scraper.list[i].strip()
-            if not my_scraper.list[i]:
-                continue
-            else:
-                print(my_scraper.list[i])
 
+        # instance of trials
+        my_trials = Trials()
+
+        # walk line per line through html
+        for j in range(0, len(my_scraper.list)):
+            my_scraper.list[j] = my_scraper.list[j].strip()
+            if my_scraper.list[j] == "":
+                continue
+            elif my_scraper.list[j] == "DRKS-ID:":
+                j += 1
+                my_trials.trial_id = my_scraper.list[j]
+            elif my_scraper.list[j] == "Title":
+                j += 1
+                while my_scraper.list[j] != "end of 1:1-Block title":
+                    my_trials.trial_title += my_scraper.list[j]
+                    j += 1
+                my_trials.trial_title = my_trials.trial_title.strip("\n")
+            elif my_scraper.list[j] == "Trial Acronym":
+                j += 1
+                my_trials.trial_acronym = my_scraper.list[j]
+            elif my_scraper.list[j] == "URL of the Trial":
+                j += 1
+                my_trials.trial_acronym = my_scraper.list[j]
+            elif my_scraper.list[j] == "Brief Summary in Lay Language":
+                j += 1
+                while my_scraper.list[j] != "end of 1:1-Block public summary":
+                    my_trials.summary_lay += my_scraper.list[j]
+                    j += 1
+                my_trials.summary_lay = my_trials.trial_title.strip("\n")
+            elif my_scraper.list[j] == "Brief Summary in Scientific Language":
+                j += 1
+                while my_scraper.list[j] != "end of 1:1-Block scientific synopsis":
+                    my_trials.summary_sci += my_scraper.list[j]
+                    j += 1
+                my_trials.summary_sci = my_trials.trial_title.strip("\n")
+            elif my_scraper.list[j] == "Organizational Data":
+                j += 1
+                while my_scraper.list[j] != "end of 1:1-Block organizational data":
+                    if my_scraper.list[j+1][-1] == ":" and my_trials.org_data:
+                        my_trials.org_data += my_scraper.list[j]
+                    else:
+                        my_trials.org_data += my_scraper.list[j].strip()
+                    j += 1
+            elif my_scraper.list[j] == "Secondary IDs":
+                j += 1
+                while my_scraper.list[j] != "end of 1:n-Block secondary IDs":
+                    if my_scraper.list[j+1][-1] == ":" and my_trials.secondary_ids:
+                        my_trials.secondary_ids += my_scraper.list[j]
+                    else:
+                        my_trials.secondary_ids += my_scraper.list[j].strip()
+                    j += 1
+            elif my_scraper.list[j] == "Health Condition or Problem studied":
+                j += 1
+                while my_scraper.list[j] != "end of 1:N-Block indications":
+                    if my_scraper.list[j+1][-1] == ":" and my_trials.health_Condition:
+                        my_trials.health_Condition += my_scraper.list[j]
+                    else:
+                        my_trials.health_Condition += my_scraper.list[j].strip()
+                    j += 1
+            elif my_scraper.list[j] == "Interventions/Observational Groups":
+                j += 1
+                while my_scraper.list[j] != "end of 1:N-Block interventions":
+                    if my_scraper.list[j+1][-1] == ":" and my_trials.interventions:
+                        my_trials.interventions += my_scraper.list[j]
+                    else:
+                        my_trials.interventions += my_scraper.list[j].strip()
+                    j += 1
+            elif my_scraper.list[j] == "Characteristics":
+                j += 1
+                while my_scraper.list[j] != "end of 1:1-Block design":
+                    if my_scraper.list[j+1][-1] == ":" and my_trials.characteristics:
+                        my_trials.characteristics += my_scraper.list[j]
+                    else:
+                        my_trials.characteristics += my_scraper.list[j].strip()
+                    j += 1
+            elif my_scraper.list[j] == "Primary Outcome":
+                j += 1
+                while my_scraper.list[j] != "end of 1:1-Block primary endpoint":
+                    if my_scraper.list[j][0] == "-" and my_trials.outcome_prim:
+                        my_trials.outcome_prim += "\n" + my_scraper.list[j].strip()
+                    else:
+                        my_trials.outcome_prim += my_scraper.list[j].strip()
+                    j += 1
+            elif my_scraper.list[j] == "Secondary Outcome":
+                j += 1
+                while my_scraper.list[j] != "end of 1:1-Block secondary endpoint":
+                    if my_scraper.list[j][0] == "-" and my_trials.outcome_sec:
+                        my_trials.outcome_sec += "\n" + my_scraper.list[j].strip()
+                    else:
+                        my_trials.outcome_sec += my_scraper.list[j].strip()
+                    j += 1
+            elif my_scraper.list[j] == "Countries of Recruitment":
+                j += 1
+                while my_scraper.list[j] != "end of 1:n-Block recruitment countries":
+                    if my_scraper.list[j+1][-1] == ":" and my_trials.countries_recruitment:
+                        my_trials.countries_recruitment += my_scraper.list[j]
+                    else:
+                        my_trials.countries_recruitment += my_scraper.list[j].strip()
+                    j += 1
+            elif my_scraper.list[j] == "Locations of Recruitment":
+                j += 1
+                while my_scraper.list[j] != "end of 1:n-Block recruitment locations":
+                    if my_scraper.list[j+1][-1] == ":" and my_trials.locations_recruitment:
+                        my_trials.locations_recruitment += my_scraper.list[j]
+                    else:
+                        my_trials.locations_recruitment += my_scraper.list[j].strip()
+                    j += 1
+
+            else:
+                # print(my_scraper.list[j])
+                pass
 
 if __name__ == "__main__":
     main()
--- trials.py
+++ trials.py
@@ -2,33 +2,29 @@ from address import Address
 
 
 class Trials(object):
-    def __init__(self, trial_id, trial_title, trial_acronym, trial_url, summary_lay, summary_sci, org_data,
-                 secondary_ids, health_condition, interventions, characteristics, outcome_prim, outcome_sec,
-                 countries_recruitment, locations_recruitment, recruitment, criteria_inclusion, criteria_inclusion_add,
-                 criteria_exclusion, add_prim_sponsor, add_sci_queries, add_pub_queries, add_mat_support, status,
-                 publications):
-        self.trial_id = trial_id
-        self.trial_title = trial_title
-        self.trial_acronym = trial_acronym
-        self.trial_url = trial_url
-        self.summary_lay = summary_lay
-        self.summary_sci = summary_sci
-        self.org_data = org_data
-        self.secondary_ids = secondary_ids
-        self.health_Condition = health_condition
-        self.interventions = interventions
-        self.characteristics = characteristics
-        self.outcome_prim = outcome_prim
-        self.outcome_sec = outcome_sec
-        self.countries_recruitment = countries_recruitment
-        self.locations_recruitment = locations_recruitment
-        self.recruitment = recruitment
-        self.criteria_inclusion = criteria_inclusion
-        self.criteria_inclusion_add = criteria_inclusion_add
-        self.criteria_exclusion = criteria_exclusion
-        self.add_prim_sponsor = add_prim_sponsor
-        self.add_sci_queries = add_sci_queries
-        self.add_pub_queries = add_pub_queries
-        self.add_mat_support = add_mat_support
-        self.status = status
-        self.publications = publications
+    def __init__(self):
+        self.trial_id = ""
+        self.trial_title = ""
+        self.trial_acronym = ""
+        self.trial_url = ""
+        self.summary_lay = ""
+        self.summary_sci = ""
+        self.org_data = ""
+        self.secondary_ids = ""
+        self.health_Condition = ""
+        self.interventions = ""
+        self.characteristics = ""
+        self.outcome_prim = ""
+        self.outcome_sec = ""
+        self.countries_recruitment = ""
+        self.locations_recruitment = ""
+        self.recruitment = ""
+        self.criteria_inclusion = ""
+        self.criteria_inclusion_add = ""
+        self.criteria_exclusion = ""
+        self.add_prim_sponsor = ""
+        self.add_sci_queries = ""
+        self.add_pub_queries = ""
+        self.add_mat_support = ""
+        self.status = ""
+        self.publications = ""
